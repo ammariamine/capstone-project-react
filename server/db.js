b96bc37ea83fbd5575f93610d7343e2184bd2269
ammariamine <ammariamine@sfax.r-iset.tn>
2024-03-05 17:11:32 -0500

    linking mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTIzNzQtYW1lcmlh@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
