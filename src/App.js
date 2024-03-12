8a7534ce10721349dc156176a8f798b78f1c6fb3
ammariamine <ammariamine@sfax.r-iset.tn>
2024-03-11 20:51:12 -0400

    adding finddoctor search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Sign_Up from './Components/Sign_up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearchIC from './Components/InstantConsultation/FindDoctorSearchIC/FindDoctorSearchIC';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 function App() {
   return (
@@ -17,7 +19,9 @@ function App() {
             <Route exact path="/" Component={Landing_Page} />
             <Route path="/singup" Component={Sign_Up} />
             <Route path="/login" Component={Login} />
-            <Route path="/instant-consultation" Component={<InstantConsultation />} />
+            <Route path="/instant-consultation" Component={ InstantConsultation } />
+            <Route path="/search/doctors" Component={FindDoctorSearch} />
+            
             </Routes>
          </switch>
         </BrowserRouter>
